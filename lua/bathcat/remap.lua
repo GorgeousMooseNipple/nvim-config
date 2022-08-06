9c0bc279c219a479beeaa709db3c3984f60b3651
GorgeousMooseNipple <orang768@gmail.com>
2022-08-06 12:12:39 +0300

    Fix mappings

--- lua/bathcat/remap.lua
+++ lua/bathcat/remap.lua
@@ -1,5 +1,13 @@
 local vnoremap = require('bathcat.keymap').vnoremap
+local nnoremap = require('bathcat.keymap').nnoremap
 
 vnoremap('<C-c>', '"+y')
 vnoremap('<C-v>', '"+p')
+
+-- Toggle between last two files
+nnoremap('<leader><leader>', '<C-^>')
+
+-- Jump to start and end of line
+nnoremap('H', '^')
+nnoremap('L', '$')
 
