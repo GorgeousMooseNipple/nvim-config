ae0bdc677324af951223fc454f91b8f26c78e3a7
GorgeousMooseNipple <orang768@gmail.com>
2022-08-10 21:56:56 +0300

    Disable rust-tools.nvim

--- lua/bathcat/lsp/settings/rust_analyzer.lua
+++ lua/bathcat/lsp/settings/rust_analyzer.lua
@@ -1,15 +1,15 @@
 -- Rust-tool options
-local tools_opts = {
-    tools = { autoSetHints = true,
-    hover_with_actions = true,
-    inlay_hints = {
-        show_parameter_hints = false,
-        parameter_hints_prefix = '',
-        other_hints_prefix = '',
-    }
-}}
-
-require('rust-tools').setup(tools_opts)
+-- local tools_opts = {
+--     tools = { autoSetHints = true,
+--     hover_with_actions = true,
+--     inlay_hints = {
+--         show_parameter_hints = false,
+--         parameter_hints_prefix = '',
+--         other_hints_prefix = '',
+--     }
+-- }}
+-- 
+-- require('rust-tools').setup(tools_opts)
 
 return {
     flags = {
