81a3fb55f5dc1ab28d105d405c27ba7d585255ee
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2022-09-22 00:40:20 +0300

    Add mappings to move lines

--- lua/bathcat/remap.lua
+++ lua/bathcat/remap.lua
@@ -11,3 +11,9 @@ nnoremap('<leader><leader>', '<C-^>')
 nnoremap('H', '^')
 nnoremap('L', '$')
 
+nnoremap('<A-j>', ':m +1<CR>')
+nnoremap('<A-k>', ':m -2<CR>')
+
+vnoremap("<A-j>", ":m '>+1<CR>gv=gv")
+vnoremap("<A-k>", ":m '<-2<CR>gv=gv")
+
