45b2e7ff0e03a56e1c6e4ca20350f4da96617be4
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2022-12-08 19:15:40 +0300

    Add vertical movement remaps

--- lua/bathcat/remap.lua
+++ lua/bathcat/remap.lua
@@ -11,9 +11,15 @@ nnoremap('<leader><leader>', '<C-^>')
 nnoremap('H', '^')
 nnoremap('L', '$')
 
+-- Move current line up and down
 nnoremap('<A-j>', ':m +1<CR>')
 nnoremap('<A-k>', ':m -2<CR>')
 
+-- Move selected block of lines up and down
 vnoremap("<A-j>", ":m '>+1<CR>gv=gv")
 vnoremap("<A-k>", ":m '<-2<CR>gv=gv")
+
+-- Move up/down half page and center view
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
 
