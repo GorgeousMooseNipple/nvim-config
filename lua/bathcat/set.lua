5795ae5b658635657d5bf59e6b9fed77c157546f
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2024-03-17 18:32:07 +0300

    Highlight on yank

--- lua/bathcat/set.lua
+++ lua/bathcat/set.lua
@@ -53,3 +53,11 @@ vim.opt.updatetime = 300
 -- Disable mouse
 vim.opt.mouse = ''
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight text on yank',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
