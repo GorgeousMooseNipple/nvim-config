154c2b2025e3e5e4bc1210b7ae72408a65fbd6de
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2024-03-21 00:29:28 +0300

    Fix compatibility between rainbow and nvim-treesitter

--- after/plugin/colour.lua
+++ after/plugin/colour.lua
@@ -9,16 +9,18 @@ if vim.fn.has('termguicolors') == 1 then
 end
 
 -- Change nvim-cmp pop-up completion menu colours
-vim.api.nvim_command[[highlight CmpItemAbbrDefault guifg=#665c54]]
-vim.api.nvim_command[[highlight CmpItemAbbrMatchDefault guifg=lightgrey]]
-vim.api.nvim_command[[highlight CmpItemAbbrMatchFuzzyDefault guifg=lightgrey]]
-vim.api.nvim_command[[highlight CmpItemKindDefault guifg=#fabd2f]]
-vim.api.nvim_command[[highlight CmpItemMenuDefault guifg=lightgrey]]
+vim.api.nvim_command [[highlight CmpItemAbbrDefault guifg=#665c54]]
+vim.api.nvim_command [[highlight CmpItemAbbrMatchDefault guifg=lightgrey]]
+vim.api.nvim_command [[highlight CmpItemAbbrMatchFuzzyDefault guifg=lightgrey]]
+vim.api.nvim_command [[highlight CmpItemKindDefault guifg=#fabd2f]]
+vim.api.nvim_command [[highlight CmpItemMenuDefault guifg=lightgrey]]
 
 -- Set up highlights for Illuminate
 -- vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'CursorLine' })
 -- vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'CursorLine' })
 -- vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'CursorLine' })
 
-vim.g.airline_section_z = '%2.p%%%#__accent_bold# ln:%l%#__restore__#%#__accent_bold#/%L%#__restore__#%#__accent_bold# cl:%-3.v%#__restore__#'
+vim.g.airline_section_z =
+'%2.p%%%#__accent_bold# ln:%l%#__restore__#%#__accent_bold#/%L%#__restore__#%#__accent_bold# cl:%-3.v%#__restore__#'
 
+vim.api.nvim_set_hl(0, '@punctuation.bracket', { link = '' })
