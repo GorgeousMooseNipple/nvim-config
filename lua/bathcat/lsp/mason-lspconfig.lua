f20b9f87bee5207a1d604851caa9bf47b5288a6f
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2025-04-22 23:00:07 +0300

    Use basedpyright because its based

--- lua/bathcat/lsp/mason-lspconfig.lua
+++ lua/bathcat/lsp/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local mason_lspconfig = require('mason-lspconfig')
 
 local lspconfig = require('lspconfig')
 
-local servers = { 'lua_ls', 'pyright', 'rust_analyzer', 'bashls', 'taplo', 'gopls' }
+local servers = { 'lua_ls', 'basedpyright', 'rust_analyzer', 'bashls', 'taplo', 'gopls' }
 
 mason_lspconfig.setup({
     ensure_installed = servers,
