ab89fdf3f42b26c1fa09ac3091855feede2af44a
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2025-04-23 13:57:14 +0300

    Change python

--- lua/bathcat/lsp/settings/robotframework_ls.lua
+++ lua/bathcat/lsp/settings/robotframework_ls.lua
@@ -1,7 +1,9 @@
 return {
     settings = {
-        python = "/home/korneevdmitriy/minicoda3/envs/robot/bin/python",
         robot = {
+            language_server = {
+                python = "/home/korneevdmitriy/minicoda3/envs/robot/bin/python"
+            },
             pythonpath = { "/home/korneevdmitriy/mnt/src/misc-autotests/hids/src", "/home/korneevdmitriy/minicoda3/envs/robot/lib/python3.10/site-packages" },
         },
     },
