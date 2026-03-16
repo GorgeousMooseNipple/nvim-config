3131abb975cc6bb89bdad87fc44a686e53379a94
GorgeousMooseNipple <dkorneev.sc@gmail.com>
2026-03-16 16:09:36 +0300

    Add keymap to open unfocused tab in netrw

--- lua/bathcat/remap.lua
+++ lua/bathcat/remap.lua
@@ -28,3 +28,19 @@ nnoremap("<C-d>", "<C-d>zz")
 -- Paste without storing replaced text in buffer
 xnoremap("<leader>p", "\"_dP")
 
+-- In netrw, open file in the new tab without focusing it
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("NetrwBgTabs", { clear = true }),
+    pattern = "netrw",
+    callback = function()
+        vim.keymap.set("n", "<C-t>", function()
+            local file = vim.fn.expand("<cfile>")
+            vim.cmd("tabedit " .. file)
+            vim.cmd("tabprevious")
+        end, {
+            buffer = true,
+            silent = true,
+            desc = "Open file in a new tab without focusing in netrw"
+        })
+    end,
+})
